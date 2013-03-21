fb9e4f8376cc5c7c90d9bcc717ce09f980780daa
Baratox <ninguem@nenhur.es>
2013-03-21 18:46:36 -0300

    Creates a special route for JavaScript assets.
    A js file, say /assets/javascripts/app/main1.js, is served from:
    * /public/javascripts/app/main1.js, in dev mode;
    * /public/javascripts-min/app/main1.js, in prod mode.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,5 +16,24 @@ public class Application extends Controller {
     public static Result page2() {
         return ok(page2.render());
     }
+
+    /**
+     * Redirects to the appropriate JavaScript asset depending on the environment.
+     * If running in <em>dev mode</em>, raw scripts are used.
+     * If running in <em>prod mode</em>, uglyfied and inlined scripts are used.
+     *
+     * @see controllers.Assets.at
+     */
+    public static Action<AnyContent> javascripts(String file) {
+        String folder = "javascripts/";
+        if (Play.isProd()) {
+            folder = "javascripts-min/";
+        }
+        
+        Logger.debug(String.format("[%s mode] JavaScript file '%s' served from '%s'.", 
+            Play.isProd() ? "prod" : "dev", file, folder + file));
+
+        return controllers.Assets.at("/public", folder + file).apply();
+    }
   
 }
