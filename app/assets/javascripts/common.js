2e68cab161970eed9fd6a35f530eafb44f45438d
Baratox <ninguem@nenhur.es>
2013-03-21 18:45:41 -0300

    Configures to user jQuery from Google's CDN, falling back to the local file.

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -8,7 +8,13 @@
 requirejs.config({
     baseUrl: '/assets/javascripts/lib',
     paths: {
-        app: '../app'
+        app: '../app',
+        
+        // JQuery is served mainly from the CDN, falling back to the local file if needed.
+        jquery: [
+            '//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min',
+            'jquery'
+        ]
     },
     shim: {
         backbone: {
